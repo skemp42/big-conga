1093d22dff033b990573afc6ca7ef01233103b3f
Jordan Spain <Jordan.Spain@comparisontech.com>
2020-04-27 09:30:34 +0100

    Add new images

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,43 @@
-import React, { ReactElement } from 'react';
-import './App.css';
-import { useState } from 'react';
-import { useEffect } from 'react';
+import React, { ReactElement } from "react";
+import "./App.css";
+import { useState } from "react";
+import { useEffect } from "react";
 
-const formatter = new Intl.NumberFormat('en-GB', {
-  style: 'currency',
-  currency: 'GBP'
+const formatter = new Intl.NumberFormat("en-GB", {
+  style: "currency",
+  currency: "GBP",
 });
 
 const headers = [
-  'A Conga a day keeps the doctor away',
-  'You call that a Conga? This is a Conga',
-  'Conga on, Conga off',
+  "A Conga a day keeps the doctor away",
+  "You call that a Conga? This is a Conga",
+  "Conga on, Conga off",
   "You can't handle the Conga",
-  'Carry on my wayward Conga',
-  'Stop, drop and Conga',
+  "Carry on my wayward Conga",
+  "Stop, drop and Conga",
   "I've got a lovely bunch of Conganuts",
   "Conga like it's 1999",
-  'Is that a Conga in your pocket or are you just happy to see me?',
-  'May the Conga be with you',
+  "Is that a Conga in your pocket or are you just happy to see me?",
+  "May the Conga be with you",
   "There's no place like Conga",
-  'Conga, my dear Watson',
+  "Conga, my dear Watson",
   "My mama always said life was like a box of chocolates. You never know what Conga you're gonna get",
   "You're gonna need a bigger Conga",
-  'Conga, we have a problem',
-  'The first rule of Conga club is you do not talk about Conga club',
-  'They call it a Conga with cheese',
-  'I wish I knew how to Conga you',
+  "Conga, we have a problem",
+  "The first rule of Conga club is you do not talk about Conga club",
+  "They call it a Conga with cheese",
+  "I wish I knew how to Conga you",
   "Help me, Obi-Wan Kenobi. You're my only Conga",
-  'Conga, not stirred',
+  "Conga, not stirred",
   "What we've got here is a failure to Conga",
-  'Say hello to my little Conga!',
-  'I love the smell of Conga in the morning',
-  'Leave the gun. Take the Conga.',
+  "Say hello to my little Conga!",
+  "I love the smell of Conga in the morning",
+  "Leave the gun. Take the Conga.",
   "I am serious. And don't call me Conga.",
   "Here's Conga!",
-  'You had me at Conga',
-  'Conga. James Conga.',
-  "Frankly my dear, I don't give a Conga"
+  "You had me at Conga",
+  "Conga. James Conga.",
+  "Frankly my dear, I don't give a Conga",
 ];
 
 const header = headers[Math.floor(Math.random() * headers.length)];
@@ -63,7 +63,14 @@ const App = (): ReactElement => {
         <h3>1</h3>
       </header>
       <div className="art">
+        <h3>Pumba</h3>
         <img src="../img/pumba.png" alt="pumba" />
+        <h3>Spiderman</h3>
+        <img src="../img/spiderman.png" alt="spiderman" />
+        <h3>Backpack</h3>
+        <img src="../img/backpack.png" alt="backpack" />
+        <h3>Desk</h3>
+        <img src="../img/desk.png" alt="desk" />
       </div>
     </div>
   );
